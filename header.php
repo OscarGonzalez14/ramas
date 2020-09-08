5557f3bc93c270dc96fbf6e1f7391491eaaf6650
Jackeline963 <jackelinedecarmena@gmail.com>
2020-09-08 14:35:35 -0600

    ima

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Este es el encabezado</p>
+    <h1>cualquier cosa</h1>
+
+
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F829929037558432179%2F&psig=AOvVaw2zID12Bru4V7zJbDULEYpn&ust=1599683576155000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCrrPmz2usCFQAAAAAdAAAAABAD">
+
+  </body>
+</html>
+
+
+
