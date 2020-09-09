4870791586859dd89e8394d73372b5c1c60a194e
OscarGonzalez14 <oscargonzalez815@gmail.com>
2020-09-09 14:33:25 -0600

    Cambios del primer commit

--- header.php
+++ header.php
@@ -5,7 +5,10 @@
     <title></title>
   </head>
   <body>
+    <!--Todos estos son cambios del primer comit-->
     <p>Este es el encabezado</p>
     <h1>cualquier cosa</h1>
+    <button type="button" name="button">Boton 1</button>
+    <!--Todos estos son cambios del primer comit-->
   </body>
 </html>
